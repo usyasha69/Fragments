812066fd5b573379b1d227af7799d4a17f3e1388
Dmitriy Usic <usyasha69@mail.ru>
2016-10-01 12:04:50 +0300

    finish add fragments

--- app/src/main/java/com/example/pk/fragments/MainActivity.java
+++ app/src/main/java/com/example/pk/fragments/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.left_fragment_container, LeftFragment.newInstance()
+                        , LeftFragment.class.getSimpleName())
+                .replace(R.id.right_fragment_container, RightFragment.newInstance()
+                        , RightFragment.class.getSimpleName())
+                .commit();
     }
 }
