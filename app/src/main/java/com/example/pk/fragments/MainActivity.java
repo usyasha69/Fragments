dc5a98308f01ff17ae3671d9ef44daaf9249316f
Dmitriy Usic <usyasha69@mail.ru>
2016-10-01 12:40:14 +0300

    use interface

--- app/src/main/java/com/example/pk/fragments/MainActivity.java
+++ app/src/main/java/com/example/pk/fragments/MainActivity.java
@@ -4,18 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private LeftFragment leftFragment;
+    private RightFragment rightFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rightFragment = RightFragment.newInstance();
+        leftFragment = LeftFragment.newInstance();
+        leftFragment.setiSender(rightFragment);
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.left_fragment_container, LeftFragment.newInstance()
+                .replace(R.id.left_fragment_container, leftFragment
                         , LeftFragment.class.getSimpleName())
-                .replace(R.id.right_fragment_container, RightFragment.newInstance()
+                .replace(R.id.right_fragment_container, rightFragment
                         , RightFragment.class.getSimpleName())
                 .commit();
+
     }
 }
